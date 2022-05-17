56a9d19c1ce173c2ca769b842658915f35322859
sexfat <bryant6831@gmail.com>
2022-05-17 16:42:49 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -105,18 +105,18 @@ function browser(done) {
         port: 3000
     });
     watch(['src/*.html' , 'src/**/*.html'], includeHTML).on('change' , reload)    // 監看html
-    //  watch('js/*.js' , moveJs)  // 監看js
-    //  watch('css/*.css' , moveCss)  // 監看js
+    watch('src/js/*.js' , moveJs)  // 監看js
+    watch(['src/images/*.*', 'src/images/**/*.*'], moveImg)  // 監看 img
     watch(['./src/sass/*.scss' ,'./src/sass/**/*.scss'], styleSass).on('change' , reload) // 監看sass
     done();
 }
 
 
 // 監看
-exports.w =  series(parallel(includeHTML,styleSass), watchfile)  
+exports.w =  series(parallel(moveJs,moveImg,includeHTML,styleSass), watchfile)  
 
 //瀏覽器同步
-exports.default =  series(parallel(includeHTML,styleSass), browser)  
+exports.default =  series(parallel(moveJs,includeHTML,styleSass,moveImg), browser)  
 
 
 
