21d3e39616a6cffdf7653d1d9f02f0e1a3098aa2
sexfat <bryant6831@gmail.com>
2022-05-26 15:25:05 +0800

    add

--- src/js/test.js
+++ src/js/test.js
@@ -1,6 +1,8 @@
 import $ from "jquery";
 import { gsap } from "gsap";
 import {cube} from './mymodule.js';
+import '../css/style.css';
+import '../css/header.css';
 
 $('body').css('background-color' , 'red');
 
@@ -13,6 +15,6 @@ gsap.to('.box' , {
     duration: 2
 })
 
-console.log(cube(10));
+console.log(cube(30));
 
 console.log('webpack ok');
--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,20 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-      }              // 出口文件
-    // module: {},              // 處裡對應模組
+      } ,             // 出口文件
+      module: {
+        rules: [{
+            // 格式
+            test: /\.css$/,
+            //順序是由下到上 css > style
+            use: [
+                'style-loader',
+                'css-loader'
+            ],
+        }]
+
+    },               // 處裡對應模組
     //plugins: [],             // 對應的插件
     //devServer: {},           // 服務器配置
-    //mode: 'development'      // 開發模式配置
+    //mode: 'production'      // 開發模式配置 production 上線   development開發
 }
